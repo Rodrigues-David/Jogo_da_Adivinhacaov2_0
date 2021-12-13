c2812c18ef6e0141861705797c5efa24982d5f21
David Rodrigues <antonio2947david@gmail.com>
2021-12-13 19:51:01 -0300

    Aplicando a escolha do nível de dificuldade.

--- Mundo02/Jogo da adivinhação v2.0 - Desafio058.py
+++ Mundo02/Jogo da adivinhação v2.0 - Desafio058.py
@@ -1,9 +1,14 @@
 import random
-computador = random.randint(1, 10)
-tentativas = 0
 print('-' * 65)
 print('{:^60}'.format('JOGO DA ADIVINHAÇÃO V2.0'))
 print('-' * 65)
+print('Escolha o nível de dificuldade: \n'
+      '[ 1 ] FÁCIL \n'
+      '[ 2 ] MÉDIO \n'
+      '[ 3 ] DIFÍCIL ')
+escolha_nivel = int(input('Escolha seu nível: '))
+computador = random.randint(1, 10)
+tentativas = 0
 print('Eu já pensei em um número, agora é sua vez, adivinhe qual número eu pensei de 1 a 10. ')
 usuario = int(input('Entre com um número inteiro: '))
 while usuario != computador:
