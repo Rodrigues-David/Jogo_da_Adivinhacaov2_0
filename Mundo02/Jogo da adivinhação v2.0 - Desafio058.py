334bfe478e798eb41c1644704dcdd0b0511f7667
David Rodrigues <antonio2947david@gmail.com>
2021-12-13 19:51:01 -0300

    Aplicando os níveis de dificuldades com condições alinhadas.

--- Mundo02/Jogo da adivinhação v2.0 - Desafio058.py
+++ Mundo02/Jogo da adivinhação v2.0 - Desafio058.py
@@ -1,17 +1,45 @@
 import random
-computador = random.randint(1, 10)
-tentativas = 0
 print('-' * 65)
 print('{:^60}'.format('JOGO DA ADIVINHAÇÃO V2.0'))
 print('-' * 65)
-print('Eu já pensei em um número, agora é sua vez, adivinhe qual número eu pensei de 1 a 10. ')
-usuario = int(input('Entre com um número inteiro: '))
-while usuario != computador:
-    if usuario < computador:
-        print('Mais... Você errou. Tente novamente.')
-    elif usuario > computador:
-        print('Menos.. Você errou. Tente novamente.')
+print('Escolha o nível de dificuldade: \n'
+      '[ 1 ] FÁCIL \n'
+      '[ 2 ] MÉDIO \n'
+      '[ 3 ] DIFÍCIL ')
+escolha_nivel = int(input('Escolha seu nível: '))
+computador = random.randint(1, 10)
+tentativas = 0
+if escolha_nivel == 1:
+    print('Este é o nível 1 e para ele têm muitas dicas')
+    print('Eu já pensei em um número, agora é sua vez, adivinhe qual número eu pensei de 1 a 10. ')
     usuario = int(input('Entre com um número inteiro: '))
-    tentativas += 1
-print(f'Parabéns, você ganhou, eu pensei no número {computador}.')
-print(f'Suas tentativas foram de {tentativas} vezes.')
+    while usuario != computador:
+        if usuario < computador:
+            print('Mais... Você errou. Tente novamente.')
+        elif usuario > computador:
+            print('Menos.. Você errou. Tente novamente.')
+        usuario = int(input('Entre com um número inteiro: '))
+        tentativas += 1
+    print(f'Parabéns, você ganhou, eu pensei no número {computador}.')
+    print(f'Suas tentativas foram de {tentativas} vezes.')
+elif escolha_nivel == 2:
+    print('Este é o nível 2 e para ele não têm dicas.')
+    print('Eu já pensei em um número, agora é sua vez, adivinhe qual número eu pensei de 1 a 10. ')
+    usuario = int(input('Entre com um número inteiro: '))
+    while usuario != computador:
+        print('Você errou. Tente novamente.')
+        usuario = int(input('Entre com um número inteiro: '))
+        tentativas += 1
+    print(f'Parabéns, você ganhou, eu pensei no número {computador}.')
+    print(f'Suas tentativas foram de {tentativas} vezes.')
+elif escolha_nivel == 3:
+    computador = random.randint(1, 20)
+    print('Este é o nível 3 e para ele não têm dicas e os números aleatorios são maiores.')
+    print('Eu já pensei em um número, agora é sua vez, adivinhe qual número eu pensei de 1 a 20. ')
+    usuario = int(input('Entre com um número inteiro: '))
+    while usuario != computador:
+        print('Você errou. Tente novamente.')
+        usuario = int(input('Entre com um número inteiro: '))
+        tentativas += 1
+    print(f'Parabéns, você ganhou, eu pensei no número {computador}.')
+    print(f'Suas tentativas foram de {tentativas} vezes.')
